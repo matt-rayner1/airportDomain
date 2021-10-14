c943af1c076bf866884f3cc61c0ec8964dfb7c3b
Matt R <mattrayner14@gmail.com>
2021-10-14 14:16:42 +0100

    finished testing and class construction

--- airport.js
+++ airport.js
@@ -8,7 +8,6 @@ class Passenger {
 
     addBag(bag) {
         this.bags.push(bag)
-        //return 'Passenger has added a bag. Weight: ${bag.weight}. Over limit: ${bag.isOverLimit()}'
     }
 }
 
@@ -39,16 +38,27 @@ class CrewMember {
         this.name = name;
         this.position = position;
         this.staffNumber = staffNumber;
+        this.bags = [];
+    }
+
+    addBag(bag) {
+        this.bags.push(bag)
     }
 }
 
 class Plane {
     constructor(type) {
         this.type = type;
+        this.passengers = [];
+        this.crewMembers = [];
     }
 
     board(passenger) {
-        //todo
+        this.passengers.push(passenger)
+    }
+
+    assign(crewMember) {
+        this.crewMembers.push(crewMember)
     }
 }
 
@@ -56,6 +66,14 @@ class Airport {
     constructor(name) {
         this.name = name;
     }
+
+    takeOff(plane) {
+        return `Plane ${plane.type} successfully took off from ${this.name}`
+    }
+
+    land(plane) {
+        return `Plane ${plane.type} successfully landed in ${this.name}`
+    }
 }
 
-module.exports = {Passenger, Bag}
+module.exports = {Passenger, Bag, CrewMember, Plane, Airport}
--- airport.test.js
+++ airport.test.js
@@ -1,11 +1,34 @@
-const {Passenger, Bag} = require('./airport.js');
+const {Passenger, Bag, CrewMember, Plane, Airport} = require('./airport.js');
 
-//Create test passenger
+//Create test passenger 1 
 const matt = new Passenger('Matt', 'Passport123', '25E')
 const mattsCabinBag = new Bag(10)
 const mattsHullBag = new Bag(20)
 matt.addBag(mattsCabinBag)
 matt.addBag(mattsHullBag)
+
+//Create test passenger 2
+const matt2 = new Passenger('Matt2', 'Passport123', '25E')
+const matt2sCabinBag = new Bag(10)
+const matt2sHullBag = new Bag(20)
+matt2.addBag(matt2sCabinBag)
+matt2.addBag(matt2sHullBag)
+
+//Create test crew member
+const debra = new CrewMember('Debra', 'Front', 12345)
+const debrasCabinBag = new Bag(8)
+debra.addBag(debrasCabinBag)
+
+//Create plane object
+const boeing737 = new Plane('Boeing 737')
+boeing737.board(matt)
+boeing737.board(matt2)
+boeing737.assign(debra)
+
+//Create Airport object
+const heathrow = new Airport('Heathrow')
+const takeOffString = heathrow.takeOff(boeing737)
+const landString = heathrow.land(boeing737)
 
 //Bag tests
 describe('Bag tests', () => {
@@ -34,5 +57,48 @@ describe('Passenger tests', () => {
 
     test('Passenger should have 2 bags correctly added', () => {
         expect(matt.bags.length).toBe(2);
+    })
+})
+
+//Crew Member test
+describe('Crew Member tests', () => {
+    test('Crew member should have name which is string', () => {
+        expect(typeof debra.name).toBe('string');
+    })
+    test('Crew member should have position which is string', () => {
+        expect(typeof debra.position).toBe('string');
+    })
+    test('Crew member should have staffNumber which is number', () => {
+        expect(typeof debra.staffNumber).toBe('number');
+    })
+
+    test('Crew member should have 1 bag correctly added', () => {
+        expect(debra.bags.length).toBe(1);
+    })
+})
+
+//Plane tests
+describe('Plane tests', () => {
+    test('Plane should have type which is string', () => {
+        expect(typeof boeing737.type).toBe('string');
+    })
+    test('Plane should have 2 passengers boarded', () => {
+        expect(boeing737.passengers.length).toBe(2);
+    })
+    test('Plane should have 1 crew member assigned', () => {
+        expect(boeing737.crewMembers.length).toBe(1);
+    })
+})
+
+//Airport tests
+describe('Airport tests', () => {
+    test('Airport should have name which is string', () => {
+        expect(typeof heathrow.name).toBe('string');
+    })
+    test('Airport takeoff message should be: Plane Boeing 737 succeffully took off from Heathrow', () => {
+        expect(takeOffString).toBe('Plane Boeing 737 successfully took off from Heathrow')
+    })
+    test('Airport landing message should be: Plane Boeing 737 succeffully landed in Heathrow', () => {
+        expect(landString).toBe('Plane Boeing 737 successfully landed in Heathrow')
     })
 })
